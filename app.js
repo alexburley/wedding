f5b5a61d9879ab8c8b9449f49036087af9ad29e4
burlzad <alexburley3@live.co.uk>
2021-01-17 21:43:26 +0000

    Login page

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
-
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -17,7 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/", (req, res, next) => {
+  res.render("index", { auth: false });
+});
 
 app.use(function (req, res, next) {
   next(createError(404));
@@ -26,7 +26,6 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
   res.status(err.status || 500);
   res.render("error");
 });
