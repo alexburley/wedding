16ca90f119a4abb7b19971007405f86748ccec6d
burlzad <alexburley3@live.co.uk>
2021-04-26 21:58:22 +0100

    Page structure

--- src/website/app.js
+++ src/website/app.js
@@ -44,6 +44,16 @@ router.get("/rsvp", (req, res) => {
   }
 });
 
+router.get("/registry", (req, res) => {
+  const token = req.cookies[authCookie];
+  try {
+    const authorised = token && jwt.verify(token, accessTokenSecret);
+    res.render("index", { authorised, registry: true });
+  } catch (err) {
+    res.render("index", { authorised: false });
+  }
+});
+
 router.post("/login", (req, res) => {
   const password = req.body.password;
   if (password === thePassword) {
