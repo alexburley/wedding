774b291bd20d5706ef42786eac05105cff5ba582
Alex Burley <alexburley3@live.co.uk>
2021-11-20 15:12:27 +0000

    Add more faqs / pre-sort

--- src/website/faqs.js
+++ src/website/faqs.js
@@ -1,38 +1,74 @@
 module.exports = [
   {
+    question: "What should I wear?",
+    answer: "",
+  },
+  {
+    question:
+      "What will the weather be like / will there be time spent outside?",
+    answer: "",
+  },
+  {
+    question: "Is there a quiet area?",
+    answer: "",
+  },
+  {
+    question: "Is there a theme?",
+    answer: "",
+  },
+  {
+    question: "Can I bring a plus one?",
+    answer: "",
+  },
+  {
+    question: "Do you cater for particular food requirements?",
+    answer: "",
+  },
+  {
+    question: "Can I film / photograph the ceremony?",
+    answer: "",
+  },
+  {
+    question: "My question is not answered here",
+    answer: "",
+  },
+  {
     question:
       "Who should I call if I can't make it to the wedding or will be late?",
-    answer:
-      "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.",
+    answer: "",
   },
   {
     question: "Can I bring my child?",
-    answer:
-      "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.",
+    answer: "",
   },
   {
     question: "What is the accessibility situation?",
-    answer:
-      "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.",
+    answer: "",
   },
   {
     question: "When is the deadline for the RSVP?",
-    answer:
-      "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.",
+    answer: "",
   },
   {
     question: "What is the parking situation?",
-    answer:
-      "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.",
+    answer: "",
   },
   {
     question: "What is the dress code?",
-    answer:
-      "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.",
+    answer: "",
   },
   {
     question: "What is the itinerary/order of the day?",
-    answer:
-      "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.",
+    answer: "",
   },
-];
+  {
+    question: "What time shall I arrive?",
+    answer: "",
+  },
+  {
+    question: "Will there be open bar and do I need cash?",
+    answer: "",
+  },
+].sort((a, b) => {
+  return a.question > b.question ? 1 : -1;
+});
