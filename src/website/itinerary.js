8cee8bc3574c788c39377d5e0723d68c842065d1
Alex Burley <alexburley3@live.co.uk>
2021-12-14 22:40:14 +0000

    Fix padding

--- src/website/itinerary.js
+++ src/website/itinerary.js
@@ -1,6 +1,9 @@
 module.exports = [
   { slot: "13:30", label: "Day guests arrive" },
   { slot: "14:00", label: "Ceremony starts" },
+  { slot: "14:30", label: "Drinks Reception" },
+  { slot: "16:00", label: "Wedding Breakfast" },
   { slot: "19:30", label: "Evening guests arrive" },
+  { slot: "21:00", label: "Evening food served" },
   { slot: "01:00", label: "Bar closes" },
 ];
